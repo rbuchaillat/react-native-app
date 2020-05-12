6d1ef2d6e6cd170f0c09db08f457f326b3751f63
Buchaillat Rémi <buchaillat.remi@hotmail.com>
2020-05-12 19:44:46 +0200

    navigation

--- front/App.js
+++ front/App.js
@@ -1,50 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+/* eslint-disable prettier/prettier */
 import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
-import {
-  Provider as PaperProvider,
-  Card,
-  Title,
-  Paragraph,
-  Button,
-  Avatar,
-} from 'react-native-paper';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
+
+import {RootNavigator} from './react/routes';
 
 const App = () => {
-  const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
-
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={{flex: 1}}>
+      <SafeAreaView style={styles.SafeAreaView}>
         <PaperProvider>
-          <Card>
-            <Card.Title
-              title="Card Title"
-              subtitle="Card Subtitle"
-              left={LeftContent}
-            />
-            <Card.Content>
-              <Title>Card title</Title>
-              <Paragraph>Card content</Paragraph>
-            </Card.Content>
-            <Card.Cover source={{uri: 'https://picsum.photos/700'}} />
-            <Card.Actions>
-              <Button>Cancel</Button>
-              <Button onPress={() => alert('ok')}>Ok</Button>
-            </Card.Actions>
-          </Card>
+          <NavigationContainer>
+            <RootNavigator />
+          </NavigationContainer>
         </PaperProvider>
       </SafeAreaView>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  SafeAreaView: {
+    flex: 1,
+  },
+});
 
 export default App;
