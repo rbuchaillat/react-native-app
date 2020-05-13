1431dc8e549c348675451fdf9b40acb879f82bc3
Buchaillat Rémi <buchaillat.remi@hotmail.com>
2020-05-13 16:30:28 +0200

    e

--- front/react/components/RegisterScreen.js
+++ front/react/components/RegisterScreen.js
@@ -1,102 +1,112 @@
-import React, { useState } from 'react'
-import { Image, View, StyleSheet, Text } from 'react-native';
-import { RadioButton } from 'react-native-paper';
+/* eslint-disable prettier/prettier */
+import React, {useState} from 'react';
+import {Image, View, StyleSheet, Text} from 'react-native';
+import {RadioButton} from 'react-native-paper';
 
 import imageLogo from '../assets/images/logo.png';
-import { Button } from './common/Button';
-import { FormTextInput } from './common/FormTextInput';
+import {Button} from './common/Button';
+import {FormTextInput} from './common/FormTextInput';
 import * as Color from '../config/colors';
 import * as Strings from '../config/strings';
 
-
 export const RegisterScreen = () => {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [isChecked, setIsChecked] = useState(new Map());
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isChecked, setIsChecked] = useState(new Map());
 
-    const handleFirstNameChange = (data) => {
-        setFirstName({firstName: data});
-    };
+  const handleFirstNameChange = (data) => {
+    setFirstName({firstName: data});
+  };
 
-    const handleLastNameChange = (data) => {
-        setLastName({lastName: data});
-    };
+  const handleLastNameChange = (data) => {
+    setLastName({lastName: data});
+  };
 
-    const handleEmailChange = (data) => {
-        setEmail({email: data});
-    };
+  const handleEmailChange = (data) => {
+    setEmail({email: data});
+  };
 
-    const handlePasswordChange = (data) => {
-        setPassword({password: data});
-    };
+  const handlePasswordChange = (data) => {
+    setPassword({password: data});
+  };
 
-    const handleRegisterPress = () => {
-        console.log('Login button pressed : ', firstName, lastName, email, password, isChecked);
-    };
+  const handleRegisterPress = () => {
+    console.log(
+      'Login button pressed : ',
+      firstName,
+      lastName,
+      email,
+      password,
+      isChecked,
+    );
+  };
 
-    return (
-        <View style={styles.container}>
-            <Image source={imageLogo} style={styles.logo} />
-            <View style={styles.form}>
-                <FormTextInput
-                    value={lastName}
-                    onChangeText={handleFirstNameChange}
-                    placeholder={Strings.LASTNAME_PLACEHOLDER}
-                />
-                <FormTextInput
-                    value={firstName}
-                    onChangeText={handleLastNameChange}
-                    placeholder={Strings.FIRSTNAME_PLACEHOLDER}
-                />
-                <FormTextInput
-                    value={email}
-                    onChangeText={handleEmailChange}
-                    placeholder={Strings.EMAIL_PLACEHOLDER}
-                />
-                <FormTextInput
-                    value={password}
-                    onChangeText={handlePasswordChange}
-                    placeholder={Strings.PASSWORD_PLACEHOLDER}
-                />
-                <View>
-                    <Text>Recruteur</Text>
-                    <RadioButton
-                        value="recruiter"
-                        status={isChecked === 'recruiter' ? 'checked' : 'unchecked'}
-                        onPress={() => { setIsChecked('recruiter') }}
-                    />
-                    <Text>Candidat</Text>
-                    <RadioButton
-                        value="candidate"
-                        status={isChecked === 'candidate' ? 'checked' : 'unchecked'}
-                        onPress={() => { setIsChecked('candidate') }}
-                    />
-                </View>
-                <Button label={Strings.LOGIN} onPress={handleRegisterPress} />
-            </View>
+  return (
+    <View style={styles.container}>
+      <Image source={imageLogo} style={styles.logo} />
+      <View style={styles.form}>
+        <FormTextInput
+          value={lastName}
+          onChangeText={handleFirstNameChange}
+          placeholder={Strings.LASTNAME_PLACEHOLDER}
+        />
+        <FormTextInput
+          value={firstName}
+          onChangeText={handleLastNameChange}
+          placeholder={Strings.FIRSTNAME_PLACEHOLDER}
+        />
+        <FormTextInput
+          value={email}
+          onChangeText={handleEmailChange}
+          placeholder={Strings.EMAIL_PLACEHOLDER}
+        />
+        <FormTextInput
+          value={password}
+          onChangeText={handlePasswordChange}
+          placeholder={Strings.PASSWORD_PLACEHOLDER}
+        />
+        <View>
+          <Text>Recruteur</Text>
+          <RadioButton
+            value="recruiter"
+            status={isChecked === 'recruiter' ? 'checked' : 'unchecked'}
+            onPress={() => {
+              setIsChecked('recruiter');
+            }}
+          />
+          <Text>Candidat</Text>
+          <RadioButton
+            value="candidate"
+            status={isChecked === 'candidate' ? 'checked' : 'unchecked'}
+            onPress={() => {
+              setIsChecked('candidate');
+            }}
+          />
         </View>
-    )
-}
+        <Button label={Strings.LOGIN} onPress={handleRegisterPress} />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: Color.WHITE,
-      alignItems: 'center',
-      justifyContent: 'space-between',
-    },
-    logo: {
-      flex: 1,
-      width: '30%',
-      resizeMode: 'contain',
-      alignSelf: 'center',
-    },
-    form: {
-      flex: 1,
-      justifyContent: 'center',
-      width: '80%',
-    },
-  });
-  
+  container: {
+    flex: 1,
+    backgroundColor: Color.WHITE,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  logo: {
+    flex: 1,
+    width: '30%',
+    resizeMode: 'contain',
+    alignSelf: 'center',
+  },
+  form: {
+    flex: 1,
+    justifyContent: 'center',
+    width: '80%',
+  },
+});
