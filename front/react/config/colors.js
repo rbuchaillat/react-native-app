33af7ca3f4a469eb1cd67eeb6f087fc46903439b
Buchaillat Rémi <buchaillat.remi@hotmail.com>
2020-05-13 13:55:50 +0200

    Homescreen update

--- front/react/config/colors.js
+++ front/react/config/colors.js
@@ -2,3 +2,6 @@
 export const WHITE = '#ffffff';
 export const DODGER_BLUE = '#428af8';
 export const SILVER = '#c0c0c0';
+export const LOGIN_BLUE = '#f13c57';
+export const REGISTER_RED = '#0dadff';
+export const HOME_YELLOW = '#ffcb2d';
