6c32124b96ce9bee60a8feee68c086425d2f384c
Buchaillat Rémi <buchaillat.remi@hotmail.com>
2020-05-14 22:48:27 +0200

    fix

--- front/react/components/HomeScreen.js
+++ front/react/components/HomeScreen.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Image, View, StyleSheet} from 'react-native';
 import {Button} from 'react-native-paper';
 
@@ -8,6 +8,15 @@ import {LOGIN, REGISTER} from '../config/strings';
 import imageLogo from '../assets/images/logo-white.png';
 
 export const HomeScreen = ({navigation}) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await fetch('https://localhost:8443/users');
+      let json = await response.json();
+      console.log(json);
+    };
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.root}>
       <View style={styles.main}>
