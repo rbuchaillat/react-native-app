8550e7bfa99d506e29cdff25f7d9aaed88490d95
najlabenchelly <chelly.n103@gmail.com>
2020-05-14 18:19:38 +0200

    Mise en place api

---
+++ api/api/src/Controller/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController
+{
+    public function __invoke(Request $data)
+    {
+        
+        return $data->get('token');
+    }
+}
